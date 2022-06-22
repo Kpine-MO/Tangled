26a32bd105da21abf6af745b0c749c74ed3feebf
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-23 01:17:38 +0300

    this commit is of the login form after styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 
-function showSignUpForm() {
+/*function showSignUpForm() {
  let show = document.querySelector('.sign-up-container');
 
  if (show.style.display == 'none') {
@@ -10,4 +10,15 @@ function showSignUpForm() {
  }
 
 }
+*/
+document.querySelector('#sign-up').addEventListener('click', function() {
+   let show = document.querySelector('.sign-up-container');
+
+   if (show.style.display == 'none') {
+      console.log('hello')
+      show.style.display="block";
+   } else {
+      show.style.display="none";
+   }
+})
 
