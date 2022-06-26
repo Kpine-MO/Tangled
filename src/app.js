8f0d6dd26c8b00470c026d40f59d3a43290d5966
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-26 03:31:34 +0300

    an addition of blog post module

--- src/app.js
+++ src/app.js
@@ -10,6 +10,17 @@ function showMenu() {
  }
 
 }
+function addPost() {
+  let btn = document.querySelector(".addpost");
+
+  if (btn.style.display == 'none') {
+   console.log('hello')
+   btn.style.display="block";
+} else {
+   btn.style.display="none";
+}
+   
+}
 
 document.querySelector('#sign-up').addEventListener('click', function() {
    let show = document.querySelector('.sign-up-container');
@@ -28,10 +39,45 @@ document.querySelector("#profile-btn").addEventListener("click",function(e) {
      .then((res) => res.text())
      .then((data) => {
       var object= JSON.parse(data);
-      console.log(object)
+      
+      for (let index = 0; index < object.length; index++) {
+         const blog =document.getElementById('users');
+         const heading = document.createElement('h2')
+         heading.innerHTML=object[index].title
+         heading.classList.add("color")
+         const btn = document.createElement("button")
+         btn.innerHTML = 'Read '
+
+         blog.appendChild(btn)
+
+
+         blog.appendChild(heading)
+
+         const body = document.createElement('p')
+         body.innerHTML=object[index].body
+
+         blog.appendChild(body)
+      }
      })
-     
-   
+})
+
+const postForm = document.querySelector("#add_post")
+
+postForm.addEventListener("submit",function(e) {
+     e.preventDefault()
+     fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 })
 
 
