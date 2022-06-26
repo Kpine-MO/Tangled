d472404aedbb5fa767cd02d9a745048caa4e4824
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-06-26 15:13:44 +0300

    commit of a app.js file that represents delete post

--- src/app.js
+++ src/app.js
@@ -33,14 +33,17 @@ document.querySelector('#sign-up').addEventListener('click', function() {
    }
 })
 
-document.querySelector("#profile-btn").addEventListener("click",function(e) {
-   e.preventDefault();
+
    fetch('https://jsonplaceholder.typicode.com/posts')
      .then((res) => res.text())
      .then((data) => {
       var object= JSON.parse(data);
+
+      // document.getElementById("users").innerHTML = object
       
       for (let index = 0; index < object.length; index++) {
+         
+
          const blog =document.getElementById('users');
          const heading = document.createElement('h2')
          heading.innerHTML=object[index].title
@@ -60,9 +63,27 @@ document.querySelector("#profile-btn").addEventListener("click",function(e) {
 
          blog.appendChild(btn)
 
+         const deleteBtn = document.createElement("button")
+         deleteBtn.innerHTML = 'delete'
+         deleteBtn.classList.add("delete")
+         deleteBtn.setAttribute("id","delete_post")
+
+         blog.appendChild(deleteBtn)
+
+         deleteBtn.addEventListener("click",function(){
+            deletePost()
+         })
+
+
+         const blogid = document.createElement('h4')
+         blogid.classList.add("hidden")
+         blogid.innerHTML=object[index].id
+
+         blog.appendChild(blogid)
+
       }
      })
-})
+
 
 const postForm = document.querySelector("#add_post")
 
@@ -94,6 +115,33 @@ postForm.addEventListener("submit",function(e) {
 
      })
 
+    function deletePost() {
+      const id = document.querySelector(".hidden").innerHTML;
+     
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+  
+      var raw = JSON.stringify({
+        "id": id
+      });
+  
+      var requestOptions = {
+        method: 'DELETE',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+  
+      fetch("https://jsonplaceholder.typicode.com/posts/" + id, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+    }
+
+function test(){
+   alert("hjkl")
+}
 
 
 
